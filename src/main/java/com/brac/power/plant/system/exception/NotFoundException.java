ce94776d12fe2e616fb2e86e062ed18e9b247773
msarker1996 <mithu.sarker@orangetoolz.com>
2024-07-14 17:37:30 +0600

    updated

--- src/main/java/com/brac/power/plant/system/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.brac.power.plant.system.exception;
-
-public class NotFoundException extends Exception {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
